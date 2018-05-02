2d0acff355cab144bc2b1e0af719850948b55d34
daseul Bae <ektmf1993@gmail.com>
2018-05-02 14:27:30 +0900

    Add input and EventHandler

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+
+    <div class="box1">
+      <img src="./assets/logo.png">
+      <h1>untitled project</h1>
+      <p>여기 까지가 app.vue 영역</p>
+    </div>
+
+    <div class="box">
+      <p>여기서 부터 라우팅 영역</p>
+      <router-view/>
+    </div>
+
   </div>
 </template>
 
@@ -19,5 +29,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.box1 {
+  border: solid blue 1px;
+}
+.box {
+  border: solid black 1px;
 }
 </style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
     <h1>{{msg}}</h1>
+    <div class="box">
+      <h2> 로그인 박스 </h2>
+      <form>
+        <div>
+          <label>ID</label>
+          <input type="text" v-model="id" @change="inputHandler" placeholder="ID"/>
+        </div>
+        <div>
+          <label>PW</label>
+          <input type="text" v-model="pw" @change="inputHandler" placeholder="PW"/>
+        </div>
+        <button type="submit" class="btn btn-default">제출</button>
+      </form>
+    </div>
+    <h1>ID : {{id}}</h1>
+    <h1>PW : {{pw}}</h1>
   </div>
 </template>
 
@@ -9,12 +25,23 @@ export default {
   name: 'Login',
   data () {
     return {
-      msg: '로그인 페이지'
+      msg: '로그인 페이지',
+      id: '',
+      pw: ''
+    }
+  },
+  methods: {
+    inputHandler: async function () {
+      console.log(this.id)
+      console.log(this.pw)
     }
   }
 }
 </script>
 
 <style scoped>
-
+.box {
+  margin: 10px;
+  border: solid blue 1px;
+}
 </style>
