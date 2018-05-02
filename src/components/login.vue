524ff2b6a2f6b0e110169188253c02006ce9d003
daseul Bae <ektmf1993@gmail.com>
2018-05-02 13:39:40 +0900

    init project untitled

---
+++ src/components/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: '로그인 페이지'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
